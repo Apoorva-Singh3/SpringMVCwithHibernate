a97bf99c97183b019cde1812cc40603763ebee47
Apoorva-Singh3 <27.apoorva@gmail.com>
2022-11-30 12:16:25 +0530

    first commit

---
+++ src/main/java/com/springmvchibernate/entity/Customer.java
@@ -0,0 +1,73 @@
+package com.springmvchibernate.entity;
+
+import jakarta.persistence.Column;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+
+    
+
+    public Customer() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getaddress() {
+        return address;
+    }
+
+    public void setaddress(String address) {
+        this.address = address;
+    }
+
+   
+
+    @Override
+    public String toString() {
+        return "Customer [id=" + id + ", firstName=" + name + ", email=" + email + ", address=" + address + "]";
+    }
+}
